949053ce59cd4d0020af0f2835c3fd8c7eff6745
Mehdi Rahmani <mehdirahmani314@gmail.com>
2026-02-22 16:16:49 +0330

    feat(hooks): add `useProducts` hook for fetching and filtering products

---
+++ src/hooks/useProducts.ts
@@ -0,0 +1,25 @@
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
+import { useProductFilterStore } from '../store/product-filter';
+import { productsService } from '../services/products/products';
+import { useDebounce } from './useDebounce';
+
+export const useProducts = () => {
+  const { category, sortBy, order, search, skip, limit } = useProductFilterStore();
+
+  const debouncedSearch = useDebounce(search, 500);
+
+  return useQuery({
+    queryKey: ['products', category, sortBy, order, debouncedSearch, skip, limit],
+    queryFn: () =>
+      productsService.findAll({
+        category,
+        sortBy,
+        order,
+        search: debouncedSearch,
+        skip,
+        limit,
+      }),
+    placeholderData: keepPreviousData,
+    staleTime: 1000 * 30,
+  });
+};
