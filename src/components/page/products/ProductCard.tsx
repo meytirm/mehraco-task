d286d27d104e3632fc9559742ef20932f3f3b0a7
Mehdi Rahmani <mehdirahmani314@gmail.com>
2026-02-23 14:56:41 +0330

    feat(components): implement ProductCard with product data

--- src/components/page/products/ProductCard.tsx
+++ src/components/page/products/ProductCard.tsx
@@ -1,3 +1,21 @@
-export function ProductCard() {
-  return <div>product card</div>;
+import type { Product } from '../../../services/products/products.type.ts';
+
+export function ProductCard({ product }: Props) {
+  return (
+    <div className="product-card">
+      <div className="product-image">
+        <img src={product.thumbnail} alt={product.title} />
+      </div>
+      <div className="product-title">{product.title}</div>
+      <div className="product-discount">
+        <div className="product-discount-percentage">{Math.ceil(product.discountPercentage)} %</div>
+        <div className="product-discount-price">{product.price}</div>
+      </div>
+      <div className="product-price">{product.price}</div>
+    </div>
+  );
+}
+
+interface Props {
+  product: Product;
 }
