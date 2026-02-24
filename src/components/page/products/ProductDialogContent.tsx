f5cac3d2b115b548171e500da00ed7530ca940c7
Mehdi Rahmani <mehdirahmani314@gmail.com>
2026-02-25 02:33:01 +0330

    feat(components): integrate ProductDialogSwiper and ProductDialogInfo into ProductDialogContent

--- src/components/page/products/ProductDialogContent.tsx
+++ src/components/page/products/ProductDialogContent.tsx
@@ -1,4 +1,6 @@
 import type { Product } from '../../../services/products/products.type.ts';
+import { ProductDialogSwiper } from './ProductDialogSwiper.tsx';
+import { ProductDialogInfo } from './ProductDialogInfo.tsx';
 
 interface Props {
   loading: boolean;
@@ -13,11 +15,14 @@ export function ProductDialogContent({ loading, product }: Readonly<Props>) {
   if (!product) {
     return <div>Product not found</div>;
   }
-
   return (
     <div className="product-dialog-content">
-      <div className="bg-red-200">1</div>
-      <div className="bg-red-200">1</div>
+      <div className="w-[50%]">
+        <ProductDialogSwiper product={product} />
+      </div>
+      <div className="w-[50%]">
+        <ProductDialogInfo product={product} />
+      </div>
     </div>
   );
 }
