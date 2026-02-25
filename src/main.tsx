5e7c50550532027f62830a1f4271270dbac76f06
Mehdi Rahmani <mehdirahmani314@gmail.com>
2026-02-26 01:33:30 +0330

    feat(styles): add skeleton.css for react-loading-skeleton styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './assets/index.css';
+import 'react-loading-skeleton/dist/skeleton.css';
 import App from './App.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
