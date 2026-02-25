320a048ec6b50b9e1848241f63cb75146742c5b1
Mehdi Rahmani <mehdirahmani314@gmail.com>
2026-02-25 18:56:23 +0330

    feat(components): enhance PriceRangeSlider with external state control and validation

--- src/components/common/PriceRangeSlider.tsx
+++ src/components/common/PriceRangeSlider.tsx
@@ -1,42 +1,66 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import RangeSlider from '../ui/RangeSlider.tsx';
 import { Input } from '../ui/Input.tsx';
 
-export default function PriceRangeSlider({ min = 0, max = 1 }: Readonly<Props>) {
-  const [values, setValues] = useState<number[]>([min, max]);
-
-  const handleMinInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+export default function PriceRangeSlider({ min, max, onChange, values }: Readonly<Props>) {
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>, minOrMax: 'min' | 'max') => {
     const target = e.target;
     if (target) {
-      setValues((values) => [Number(target.value), values[1]]);
+      if (minOrMax === 'min') {
+        if (+target.value < min) {
+          onChange([min, values[1]]);
+          return;
+        }
+        if (+target.value > max || +target.value > values[1]) {
+          e.preventDefault();
+          return;
+        }
+      } else {
+        if (+target.value > max) {
+          onChange([values[0], max]);
+          return;
+        }
+        if (+target.value < min || +target.value < values[0]) {
+          e.preventDefault();
+          return;
+        }
+      }
+      const newValues = { min: [+target.value, values[1]], max: [values[0], +target.value] };
+      onChange(newValues[minOrMax]);
     }
   };
 
-  const handleMaxInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const target = e.target;
-    if (target) {
-      setValues((values) => [values[0], Number(target.value)]);
-    }
-  };
+  useEffect(() => {
+    onChange([min, max]);
+    console.log(min, max, values);
+    return () => {
+      console.log('cleanup');
+    };
+  }, [min, max]);
 
   return (
     <div className="price-range-slider">
-      <RangeSlider values={values} onChange={setValues} min={min} max={max} step={1} />
+      <RangeSlider
+        values={values}
+        onChange={(value) => {
+          onChange(value);
+        }}
+        min={min}
+        max={max}
+        step={1}
+      />
 
       <div className="price-range-slider-inputs">
         <Input
           value={values[0]}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleMinInput(e)}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            handleInput(e, 'min');
+          }}
         />
         <Input
           value={values[1]}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleMaxInput(e)}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleInput(e, 'max')}
         />
-      </div>
-
-      <div className="price-range-slider-values">
-        <span>Min: ${values[0]}</span>
-        <span>Max: ${values[1]}</span>
       </div>
     </div>
   );
@@ -45,4 +69,6 @@ export default function PriceRangeSlider({ min = 0, max = 1 }: Readonly<Props>) 
 interface Props {
   min: number;
   max: number;
+  onChange: (values: number[]) => void;
+  values: number[];
 }
