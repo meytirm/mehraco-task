70111fc98b8797a57333cd40c41f2431cb5483f2
Mehdi Rahmani <mehdirahmani314@gmail.com>
2026-02-27 11:02:41 +0330

    feat(components): improve slider and filter drawer usability with layout and input updates

--- src/components/page/products/FilterDrawer.tsx
+++ src/components/page/products/FilterDrawer.tsx
@@ -24,19 +24,23 @@ export default function FilterDrawer({ loading = false }: Readonly<Props>) {
           className="
             fixed bottom-0 left-0 right-0
             bg-white rounded-t-2xl
-            p-6 h-[70vh]
+            p-6 h-[70vh] flex flex-col
           "
         >
           <div className="mx-auto w-12 h-1.5 bg-gray-300 rounded mb-4" />
-
-          <div className="flex justify-between items-center mb-4">
+          {/* header (fixed) */}
+          <div className="flex justify-between items-center mb-4 shrink-0">
             <Drawer.Title className="text-lg font-semibold">Filters</Drawer.Title>
 
             <Drawer.Close>
               <XIcon className="text-muted hover:text-red-600" />
             </Drawer.Close>
           </div>
-          <ProductsFilter loading={loading} />
+
+          {/* scrollable area */}
+          <div className="flex-1 overflow-y-auto">
+            <ProductsFilter loading={loading} />
+          </div>
         </Drawer.Content>
       </Drawer.Portal>
     </Drawer.Root>
